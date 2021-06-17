403ecb931c883f0ef6b90db516641ab156db8b81
TiMacedoC <tiagomacedo99@hotmail.com>
2021-06-17 18:23:58 -0300

    projeto finalizado na versão 1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Projeto de CSS dinâmico
+
+==========================================================
+Projeto rodando: https://codepen.io/Timacedoc/full/ZEePWVX
--- script.js
+++ script.js
@@ -1,7 +1,47 @@
 const root = document.documentElement;
 
+const colorDrawerContent =
+    `<div class="eachButton" style="background-color: #00ff00;" onclick="changeColor('#00ff00')"></div>
+<div class="eachButton" style="background-color: #0011ff;" onclick="changeColor('#0011ff')"></div>
+<div class="eachButton" style="background-color: #eeff00;" onclick="changeColor('#eeff00')"></div>
+<div class="eachButton" style="background-color: #ff00dd;" onclick="changeColor('#ff00dd')"></div>
+<div class="eachButton" style="background-color: #ff0000;" onclick="changeColor('#ff0000')"></div>
+<div class="eachButton" style="background-color: #000000;" onclick="changeColor('#000000')"></div>
+<div class="eachButton" style="background-color: #00d9ff;" onclick="changeColor('#00d9ff')"></div>
+<div class="eachButton" style="background-color: #ffa600;" onclick="changeColor('#ffa600')"></div>`
+
 
 function changeValue(id, value) {
     const sufix = (id == "radius") ? "%" : "px";
     root.style.setProperty(`--${id}`, value + sufix);
+}
+
+function openColorDrawer() {
+
+    const colorDrawer = document.getElementById("buttons");
+
+    console.log(colorDrawer);
+
+    colorDrawer.textContent = "";
+    colorDrawer.insertAdjacentHTML("afterbegin", colorDrawerContent)
+
+    colorDrawer.style.setProperty("background-color", "#aaaaaa");
+}
+
+function changeColor(newColor) {
+    const closedColorDrawer = document.getElementById("buttons");
+
+    root.style.setProperty("--boxColor", newColor);
+
+    closedColorDrawer.textContent = "";
+
+    closedColorDrawer.insertAdjacentHTML
+        ("afterbegin",
+            `<div 
+            class="eachButton" 
+            style="background-color: ${newColor};" 
+            onclick= 'openColorDrawer()'>
+        </div>`)
+
+    closedColorDrawer.style.setProperty("background-color", "");
 }
